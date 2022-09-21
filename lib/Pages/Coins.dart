329a5c1cc969675189d4a64a015b7bb5477b8dac
Defcon41 <42962516+Kaleab41@users.noreply.github.com>
2022-09-22 01:14:10 +0300

    Coin Card Completed

--- lib/Pages/Coins.dart
+++ lib/Pages/Coins.dart
@@ -60,7 +60,49 @@ class CoinsPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-                height: 60, width: double.infinity, color: Colors.red),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Padding(
+                    padding: const EdgeInsets.all(11.0),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.black,
+                            radius: 20,
+                          ),
+                          SizedBox(width: 15),
+                          Column(
+                            children: [
+                              Text('Bitcoin',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                              Text('Btc +2411.14',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 13)),
+                            ],
+                          ),
+                          SizedBox(width: 90),
+                          Column(
+                            children: [
+                              Text('Bitcoin',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20)),
+                              Text('Btc +2411.14',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 13)),
+                            ],
+                          )
+                        ]),
+                  ),
+                ),
+                height: 65,
+                width: double.infinity,
+                color: Colors.red),
           )
         ],
       ),
