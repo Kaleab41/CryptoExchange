87d21403c40edd0da39e1e173ae58271670c3b91
Defcon41 <42962516+Kaleab41@users.noreply.github.com>
2022-09-22 01:46:20 +0300

    Final card touches complete api integration for coin page remaining

--- lib/Pages/Coins.dart
+++ lib/Pages/Coins.dart
@@ -57,52 +57,55 @@ class CoinsPage extends StatelessWidget {
             ],
           ),
           SizedBox(height: 50),
+          Text('Wallet'),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(5.0),
             child: Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
                 child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25),
-                  child: Padding(
-                    padding: const EdgeInsets.all(11.0),
-                    child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        // crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          CircleAvatar(
-                            backgroundColor: Colors.black,
-                            radius: 20,
-                          ),
-                          SizedBox(width: 15),
-                          Column(
-                            children: [
-                              Text('Bitcoin',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20)),
-                              Text('Btc +2411.14',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 13)),
-                            ],
-                          ),
-                          SizedBox(width: 90),
-                          Column(
-                            children: [
-                              Text('Bitcoin',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20)),
-                              Text('Btc +2411.14',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 13)),
-                            ],
-                          )
-                        ]),
-                  ),
+                  padding: const EdgeInsets.only(
+                      left: 0, right: 11, top: 11, bottom: 11),
+                  child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      // crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.black,
+                          radius: 20,
+                        ),
+                        Column(
+                          children: [
+                            Text('Bitcoin',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20)),
+                            Text('Btc +2411.14',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.normal,
+                                    fontSize: 13)),
+                          ],
+                        ),
+                        SizedBox(width: 90),
+                        Column(
+                          children: [
+                            Text('0.15277',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20)),
+                            Text(
+                              '+21.14%',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 13,
+                                color: Colors.green,
+                              ),
+                            ),
+                          ],
+                        )
+                      ]),
                 ),
-                height: 65,
-                width: double.infinity,
-                color: Colors.red),
+              ),
+              height: 65,
+              width: double.infinity,
+            ),
           )
         ],
       ),
